485895d3f08ca988ce55bc8c8b877c410b90e853
ilkaydnc <ilkaydinc1@gmail.com>
2020-07-04 17:33:20 +0300

    Add createWork and updateWork methods

--- components/AddWork.vue
+++ components/AddWork.vue
@@ -1,59 +1,53 @@
 <template>
   <v-card-text>
     <v-container>
-      <v-row>
+      <v-row
+        ref="add_work"
+        tag="form"
+        lazy-validation
+        @submit.prevent="submitForm"
+      >
         <v-col cols="12" sm="12" md="6">
           <v-select
-            :value="add_work_form.partner"
+            v-model="partnerId"
             :items="partners"
+            item-text="name"
+            item-value="id"
             label="Usta Seçiniz*"
             prepend-icon="location_on"
             required
-            @input="formInput($event, 'add_work', 'partner')"
+            clearable
+            :rules="[(v) => !!v || 'Bu kısım boş bırakılamaz.']"
+            @input="getSectorsWithPartnerId"
           />
         </v-col>
         <v-col cols="12" sm="12" md="6">
           <v-select
-            :value="add_work_form.location"
-            :items="locations"
-            label="Şehir Seçiniz*"
-            prepend-icon="location_on"
-            required
-            @input="formInput($event, 'add_work', 'location')"
-          />
-        </v-col>
-        <v-col cols="12" sm="12" md="6">
-          <v-select
-            :value="add_work_form.sector"
+            v-model="sectorId"
             :items="sectors"
-            attach
+            item-text="name"
+            item-value="id"
             prepend-icon="work"
             label="Sektör Seçiniz*"
-            multiple
-            @input="formInput($event, 'add_work', 'sector')"
+            clearable
+            :disabled="!partnerId"
+            :rules="[(v) => !!v || 'Bu kısım boş bırakılamaz.']"
           />
         </v-col>
         <v-col cols="12" sm="12" md="6">
           <v-text-field
-            :value="add_work_form.customer_phone"
-            label="Müşteri Telefonu Giriniz*"
-            prepend-icon="phone"
-            required
-            @input="formInput($event, 'add_work', 'customer_phone')"
-          ></v-text-field>
-        </v-col>
-        <v-col cols="12" sm="12" md="6">
-          <v-text-field
-            :value="add_work_form.amount"
+            v-model="amount"
             label="İş Tutarını Giriniz*"
             prepend-icon="money"
+            type="number"
             required
-            @input="formInput($event, 'add_work', 'amount')"
+            clearable
+            :rules="[(v) => !!v || 'Bu kısım boş bırakılamaz.']"
           ></v-text-field>
         </v-col>
         <v-col cols="12" sm="12" md="6">
           <v-menu
-            v-model="menu2"
+            v-model="menu"
             :close-on-content-click="false"
             :nudge-right="40"
             transition="scale-transition"
@@ -62,7 +56,7 @@
           >
             <template v-slot:activator="{ on, attrs }">
               <v-text-field
-                :value="add_work_form.date"
+                v-model="date"
                 label="İş Yapılma Tarihi*"
                 prepend-icon="event"
                 readonly
@@ -71,16 +65,16 @@
               ></v-text-field>
             </template>
             <v-date-picker
-              :value="add_work_form.date"
+              v-model="date"
               locale="tr-TR"
               :max="new Date().toISOString().substr(0, 10)"
-              @input="formInput($event, 'add_work', 'date')"
+              @input="menu = false"
             ></v-date-picker>
           </v-menu>
         </v-col>
         <v-col />
         <v-col cols="12" sm="12" md="4" align-self="end">
-          <v-btn class="my-3" color="primary" block>
+          <v-btn type="submit" class="my-3" color="primary" block>
             <v-icon dark left>add</v-icon>
             Ekle
           </v-btn>
@@ -92,16 +86,104 @@
 </template>
 
 <script>
+/* eslint-disable vue/require-prop-types */
+/* eslint-disable vue/prop-name-casing */
 import { mapState } from 'vuex'
-import { handleForm } from '../store'
+import { CREATE_WORK, UPDATE_WORK } from '../graphql/queries'
+import { handleError, handleLoading } from '../store'
+import graphqlClient from '~/graphql'
 export default {
   name: 'Partners',
+  props: ['work'],
+  data({ work }) {
+    return {
+      partnerId: work && work.partner.id,
+      locationId: work && work.location.id,
+      sectorId: work && work.sector.id,
+      amount: work && work.amount,
+      date:
+        (work && new Date(work.date).toISOString().substr(0, 10)) ||
+        new Date().toISOString().substr(0, 10),
+      menu: false,
+      sectors: work ? work.partner.sectors : [],
+      isEdit: Boolean(work)
+    }
+  },
   computed: {
-    ...mapState(['locations', 'sectors', 'partners', 'add_work_form'])
+    ...mapState(['partners'])
   },
   methods: {
-    formInput(value, name, key) {
-      this.$store.commit(handleForm, { value, name, key })
+    getSectorsWithPartnerId(e) {
+      if (!e) {
+        this.sectors = []
+        return
+      }
+      const partner = this.partners.find((item) => item.id === e)
+      this.sectors = partner.sectors
+      this.locationId = partner.location.id
+    },
+    async submitForm(e) {
+      const { partnerId, sectorId, amount, date } = this
+      if (!partnerId && !sectorId && !amount && !date) return
+      this.$store.commit(handleLoading, { name: 'modal', value: true })
+      if (this.isEdit) {
+        await this.updateWork(this)
+      } else {
+        await this.createWork(this)
+      }
+      this.$store.commit(handleLoading, { name: 'modal', value: false })
+    },
+    async createWork(work) {
+      const { partnerId, locationId, sectorId, amount, date } = work
+      try {
+        await graphqlClient.mutate({
+          mutation: CREATE_WORK,
+          variables: {
+            createWorkInput: {
+              partnerId,
+              sectorId,
+              locationId,
+              amount: +amount,
+              date: new Date(date).toISOString()
+            }
+          }
+        })
+        this.$store.dispatch('getData')
+        this.$store.dispatch('toggleModal', {
+          value: false,
+          name: null,
+          title: null
+        })
+      } catch (error) {
+        this.$store.commit(handleError, error.message)
+        throw new Error(error)
+      }
+    },
+    async updateWork(work) {
+      const { partnerId, locationId, sectorId, amount, date } = work
+      try {
+        await graphqlClient.mutate({
+          mutation: UPDATE_WORK,
+          variables: {
+            updateWorkInput: {
+              id: this.work.id,
+              partnerId,
+              sectorId,
+              locationId,
+              amount: +amount,
+              date: new Date(date).toISOString()
+            }
+          }
+        })
+        this.$store.dispatch('getData')
+        this.$store.dispatch('toggleModal', {
+          value: false,
+          name: null,
+          title: null
+        })
+      } catch (error) {
+        this.$store.commit(handleError, error.message)
+      }
     }
   }
 }
