70d1b43eee7fdd1acf15170e06a9cc4e5a5af0be
ilkaydnc <ilkaydinc1@gmail.com>
2020-07-10 17:33:23 +0300

    Add auto login and Update form state

--- pages/login.vue
+++ pages/login.vue
@@ -2,16 +2,15 @@
   <v-row align="center" justify="center" style="height: 100%">
     <v-col cols="12" sm="12" md="8" lg="4" xl="3">
       <v-alert
-        v-model="error"
+        :value="Boolean(error_message)"
         transition="slide-x-transition"
         type="error"
-        dismissible
       >
-        {{ errorMessage }}
+        {{ error_message }}
       </v-alert>
       <v-card class="elevation-4">
         <v-toolbar color="primary" dark>
-          <v-toolbar-title>Giriş Formu</v-toolbar-title>
+          <v-toolbar-title>Giriş Yap</v-toolbar-title>
           <v-spacer></v-spacer>
         </v-toolbar>
         <v-card-text>
@@ -22,30 +21,30 @@
             @submit.prevent="login"
           >
             <v-text-field
-              v-model="loginForm.username"
+              v-model="email"
               label="Kullanıcı Adı"
-              name="username"
+              name="email"
               prepend-icon="mdi-account"
               type="text"
               color="primary"
-              :rules="[rules.required]"
+              :rules="[(v) => !!v || 'Bu kısım boş bırakılamaz.']"
             />
             <v-text-field
               id="password"
-              v-model="loginForm.password"
+              v-model="password"
               label="Parola"
               name="password"
               prepend-icon="mdi-lock"
               type="password"
               color="primary"
-              :rules="[rules.password]"
+              :rules="[(v) => !!v || 'Bu kısım boş bırakılamaz.']"
             />
           </v-form>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn type="submit" form="loginForm" color="primary" dark>
-            Giriş Yap
+            Giriş
           </v-btn>
         </v-card-actions>
       </v-card>
@@ -54,46 +53,39 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import { CURRENT_USER } from '../graphql/queries'
+import { setAuth, setUser } from '../store'
+import graphqlClient from '~/graphql'
 export default {
   name: 'Login',
   data() {
     return {
-      loginForm: {
-        username: 'test',
-        password: 'Qq12345678.'
-      },
-      rules: {
-        required: (value) => !!value || 'Kullanıcı adı boş olamaz.',
-        password: (value) => {
-          // eslint-disable-next-line no-useless-escape
-          const pattern = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*?[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/])[a-zA-Z0-9-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]*/
-          return (
-            pattern.test(value) ||
-            'Şifre minimum 8 karakter ve en az bir tane büyük, küçük harf, rakam ve sembol içermelidir.'
-          )
-        }
-      },
-      valid: true,
-      error: false,
-      errorMessage: ''
+      email: 'ilkay@mail.com',
+      password: '123456789',
+      valid: true
     }
   },
+  computed: {
+    ...mapState(['error_message'])
+  },
+  async mounted() {
+    try {
+      const { data } = await graphqlClient.query({
+        query: CURRENT_USER,
+        fetchPolicy: 'network-only'
+      })
+      this.$store.commit(setAuth, true)
+      this.$store.commit(setUser, data.me)
+      this.$router.push('/')
+    } catch (error) {}
+  },
   methods: {
-    async login() {
-      await this.validate()
-      if (!this.valid) return null
+    login() {
+      const { email, password } = this
+      if (!email && !password) return
 
-      try {
-        await this.$auth.loginWith('local', {
-          data: this.loginForm
-        })
-      } catch (error) {
-        this.error = true
-        this.errorMessage = error.message
-      }
-    },
-    validate() {
-      this.$refs.form.validate()
+      this.$store.dispatch('signIn', { email, password })
     }
   }
 }
