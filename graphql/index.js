d307c30f9019aa35fd931b41ca65f667b377be0e
ilkaydnc <ilkaydinc1@gmail.com>
2020-07-10 20:24:22 +0300

    Add error link to apollo client

--- graphql/index.js
+++ graphql/index.js
@@ -2,9 +2,11 @@ import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { setContext } from 'apollo-link-context'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { onError } from 'apollo-link-error'
+import { ApolloLink } from 'apollo-link'
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql'
+  uri: process.env.GRAPHQL_URL
 })
 
 const authLink = setContext((_, { headers }) => {
@@ -19,7 +21,19 @@ const authLink = setContext((_, { headers }) => {
   }
 })
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ extensions }) => {
+      if (extensions.exception.response.status === 401) {
+        localStorage.setItem('token', undefined)
+        window.location.reload()
+      }
+    })
+
+  if (networkError) throw new Error(`[Network error]: ${networkError}`)
+})
+
 export default new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: ApolloLink.from([authLink, errorLink, httpLink]),
   cache: new InMemoryCache()
 })
